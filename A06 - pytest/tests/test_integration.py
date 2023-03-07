9580e2cf4b6016c205ac406ba167c46ebccf8879
Augusto Pedrini <pedrinias@gmail.com>
2023-03-07 19:18:08 -0300

    first commit

--- A06 - pytest/tests/test_integration.py
+++
@@ -1,8 +0,0 @@
-from apis import DaySummaryApi
-import datetime
-
-class TestDaySummaryApi:
-    def test_get_data(self):
-        actual = DaySummaryApi(coin = 'BTC').get_data(date = datetime.date(2021, 1, 1)).get("date")
-        expected = '2021-01-01'
-        assert actual == expected
